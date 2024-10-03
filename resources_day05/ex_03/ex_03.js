f3155e1b95de3270acb6d43073a6e7440768e71a
Antoine Chopard <antoinechopard@MacBook-Pro-de-Antoine.local>
2024-10-03 14:56:14 +0200

    Finish ex03

---
+++ resources_day05/ex_03/ex_03.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var paragraphs = document.querySelectorAll('p');
+var lastParagraph = paragraphs[paragraphs.length - 1];
+var newParagraph = document.createElement('p');
+
+newParagraph.textContent = 'Now featuring a headphone jack!';
+lastParagraph.insertAdjacentElement('afterend', newParagraph);
+});
